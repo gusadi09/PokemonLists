0d0ad1ebc846d88fe1d9d2b324dca9e4c963c4bb
Agus Adi <gusadi872@gmail.com>
2023-09-14 16:49:19 +0700

    [POKEDETAIL] add pokemon detail model

--- PokemonDataSource/Sources/PokemonDataSource/Entities/Pokemon/Response/PKPokemon.swift
+++ PokemonDataSource/Sources/PokemonDataSource/Entities/Pokemon/Response/PKPokemon.swift
@@ -57,3 +57,165 @@ public extension PKPokemonList {
         sample.toData()
     }
 }
+
+public struct PKMove: Codable {
+    public let name: String?
+    
+    public init(name: String?) {
+        self.name = name
+    }
+}
+
+public extension PKMove {
+    static var sample: PKMove {
+        PKMove(name: "Test Move")
+    }
+    
+    static var sampleData: Data {
+        sample.toData()
+    }
+}
+
+public struct PKSprite: Codable {
+    public let other: PKOtherSprite?
+    
+    public init(other: PKOtherSprite?) {
+        self.other = other
+    }
+}
+
+public extension PKSprite {
+    static var sample: PKSprite {
+        PKSprite(other: PKOtherSprite.sample)
+    }
+    
+    static var sampleData: Data {
+        sample.toData()
+    }
+}
+
+public struct PKOtherSprite: Codable {
+    public let dreamWorld: PKDreamWorldSprite?
+    
+    public init(dreamWorld: PKDreamWorldSprite?) {
+        self.dreamWorld = dreamWorld
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case dreamWorld = "dream_world"
+    }
+}
+
+public extension PKOtherSprite {
+    static var sample: PKOtherSprite {
+        PKOtherSprite(dreamWorld: PKDreamWorldSprite.sample)
+    }
+    
+    static var sampleData: Data {
+        sample.toData()
+    }
+}
+
+public struct PKDreamWorldSprite: Codable {
+    public let frontDefault: String?
+    
+    public init(frontDefault: String?) {
+        self.frontDefault = frontDefault
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
+
+public extension PKDreamWorldSprite {
+    static var sample: PKDreamWorldSprite {
+        PKDreamWorldSprite(frontDefault: "sprite")
+    }
+    
+    static var sampleData: Data {
+        sample.toData()
+    }
+}
+
+public struct PKType: Codable {
+    public let slot: Int?
+    public let type: PKTypeData?
+    
+    public init(slot: Int?, type: PKTypeData?) {
+        self.slot = slot
+        self.type = type
+    }
+}
+
+public extension PKType {
+    static var sample: PKType {
+        PKType(slot: 1, type: PKTypeData.sample)
+    }
+    
+    static var sampleData: Data {
+        sample.toData()
+    }
+}
+
+public struct PKTypeData: Codable {
+    public let name: String?
+    public let url: String?
+    
+    public init(name: String?, url: String?) {
+        self.name = name
+        self.url = url
+    }
+}
+
+public extension PKTypeData {
+    static var sample: PKTypeData {
+        PKTypeData(name: "test type", url: "type")
+    }
+    
+    static var sampleData: Data {
+        sample.toData()
+    }
+}
+
+public struct PKPokemonDetail: Codable {
+    public let name: String?
+    public let sprites: [PKSprite]?
+    public let types: [PKType]?
+    public let moves: [PKMove]?
+    public let weight: UInt?
+    public let height: UInt?
+    
+    public init(
+        name: String?,
+        sprites: [PKSprite]?,
+        types: [PKType]?,
+        moves: [PKMove]?,
+        weight: UInt?,
+        height: UInt?
+    ) {
+        self.name = name
+        self.sprites = sprites
+        self.types = types
+        self.moves = moves
+        self.weight = weight
+        self.height = height
+    }
+}
+
+public extension PKPokemonDetail {
+    static var sample: PKPokemonDetail {
+        PKPokemonDetail(
+            name: "test",
+            sprites: [PKSprite.sample],
+            types: [PKType.sample],
+            moves: [PKMove.sample],
+            weight: 10,
+            height: 10
+        )
+    }
+    
+    static var sampleData: Data {
+        sample.toData()
+    }
+}
